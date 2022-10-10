4eda4dc0b47f3b03cf8cb29c72ef51e1be0c19e7
KhizhnyakAV <112726757+KhizhnyakAV@users.noreply.github.com>
2022-10-10 04:12:03 +0300

    Add files via upload

---
+++ Program41.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Введите числа (окончание ввода - пустой ввод");
+
+int count = 0;
+while (Console.ReadLine() != "")
+{
+count++;
+}
+
+System.Console.WriteLine(count);
---
+++ Program43.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2");
+System.Console.WriteLine("Введите k1:");
+double k1 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите b1:");
+double b1 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите k2:");
+double k2 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите b2:");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+x = Math.Round(x,2);
+double y = k1*x+b1;
+y = Math.Round(y,2);
+
+System.Console.WriteLine($"({x};{y})");
